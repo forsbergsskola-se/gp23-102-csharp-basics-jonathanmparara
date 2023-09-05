ff62e0df8ccff3ed71fda5527d06729950871400
jonathan.mparara <jonathan.mparara@student.forsbergsskola.se>
2023-09-05 15:43:02 +0200

    made two new  program p6 and p7 to learn how to use different variables

--- P6Datatypes/Program.cs
+++ P6Datatypes/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string outputString = "Helo World";
-Console.Write(outputString);
+Console.WriteLine(outputString);
 
-float decimalValue = .3f;
+float decimalValue = (.3f);
 Console.WriteLine(decimalValue);
 
 bool isAwsome = true;
     Console.WriteLine(isAwsome);
+
+byte hexByte = 0xa7;
+Console.WriteLine(hexByte);
---
+++ P7Input/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What's your name?");
+string name = Console.ReadLine();
+Console.Write("Hello ");
+    Console.WriteLine(name);
