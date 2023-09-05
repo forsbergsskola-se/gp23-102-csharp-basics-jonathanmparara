e52d3b643f00cc4d9ffc2e037dcd466a0c8404ef
jonathan.mparara <jonathan.mparara@student.forsbergsskola.se>
2023-09-05 14:13:26 +0200

    created to new project to learn how to use different variables

---
+++ P5Variables/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string number = "5";
+Console.WriteLine(number);
+number = "10";
+Console.WriteLine(number);
---
+++ P6Datatypes/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string outputString = "Helo World";
+Console.Write(outputString);
+
+float decimalValue = .3f;
+Console.WriteLine(decimalValue);
+
+bool isAwsome = true;
+    Console.WriteLine(isAwsome);
