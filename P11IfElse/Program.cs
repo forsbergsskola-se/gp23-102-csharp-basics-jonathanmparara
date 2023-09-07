2030b23703d5df3938392938ec8ebc07100552e7
jonathan.mparara <jonathan.mparara@student.forsbergsskola.se>
2023-09-07 16:23:48 +0200

    learned about if and else

---
+++ P11IfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("What's your age?");
+
+int age = int.Parse(Console.ReadLine()!);
+
+if (age < 12) 
+{
+Console.WriteLine("awww you are a child HAHAHAHA");
+    
+}
+else if (age <= 19) 
+
+{
+    Console.WriteLine("Not bad, you are a teenager"); 
+}
+else
+{
+    Console.WriteLine("you are an adult");
+}
+    
+
+
+
